172678d7dd69e9765cf2a71958c497853b7fa135
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-08-08 12:59:16 +0330

    Added bottom items of editor

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Toolbar from "@/component/toolbar";
 import useEditorStore from "@/store/editor";
 import useNotesStore from "@/store/notes";
 import { useRef } from "react";
-import { Button } from "@/component/ui/button";
+import { Button, buttonVariants } from "@/component/ui/button";
 import {
    Empty,
    EmptyContent,
@@ -19,10 +19,11 @@ import { Heading } from "lucide-react";
 import { Kbd, KbdGroup } from "@/component/ui/kbd";
 import useSideBarStore from "@/store/sidebar";
 import { ScrollArea } from "@/component/ui/scroll-area";
+import { cn } from "@/lib/util";
 
 export default function HomePage() {
    const editorRef = useRef<HTMLDivElement | null>(null);
-   const { disabled, activeNoteId } = useEditorStore();
+   const { disabled, activeNoteId, mode } = useEditorStore();
    const { notes, updateNote } = useNotesStore();
    const { toggle } = useSideBarStore();
 
@@ -30,18 +31,48 @@ export default function HomePage() {
 
    return (
       <section className="relative z-0">
-         <Toolbar className="fixed top-2 right-2 z-10" />
+         <Toolbar className="fixed top-2 right-2 z-10 shadow-lg shadow-black/20" />
          <SideBar className="z-30" />
          <main onClick={() => editorRef.current?.focus()}>
             {activeNote && activeNoteId ? (
-               <Editor
-                  initialMarkdown={activeNote.content}
-                  disabled={disabled}
-                  ref={editorRef}
-                  onChange={(content) => {
-                     updateNote(activeNoteId, activeNote.title, content);
-                  }}
-               />
+               <>
+                  <div className="flex items-center justif-between gap-2 z-10 fixed bottom-2 right-2 flex-wrap">
+                     <div
+                        className={buttonVariants({
+                           size: "default",
+                           variant: "outline",
+                        })}
+                     >
+                        {activeNote.title}.md
+                     </div>
+                     {mode !== "disabled" && (
+                        <div
+                           className={buttonVariants({
+                              variant: "outline",
+                              size: "default",
+                              className: cn(
+                                 "border-dashed !border-current transition-colors duration-300",
+                                 mode === "normal"
+                                    ? "text-sky-500"
+                                    : mode === "insert"
+                                      ? "text-rose-500"
+                                      : "text-amber-500",
+                              ),
+                           })}
+                        >
+                           -- {mode.toUpperCase()} --
+                        </div>
+                     )}
+                  </div>
+                  <Editor
+                     initialMarkdown={activeNote.content}
+                     disabled={disabled}
+                     ref={editorRef}
+                     onChange={(content) => {
+                        updateNote(activeNoteId, activeNote.title, content);
+                     }}
+                  />
+               </>
             ) : (
                <ScrollArea className={"h-dvh overflow-auto p-4"}>
                   <div className="min-h-dvh flex items-center justify-center  max-w-2xl mx-auto">
